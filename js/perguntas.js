75a3f0796c8f9807c2b7a495d53a5ce75c2b182d
Bruna Eurich <schinemann.bruna@escola.pr.gov.br>
2026-09-24 11:26:17 -0300

    h

--- js/perguntas.js
+++ js/perguntas.js
@@ -6,11 +6,13 @@ export const perguntas = [
                 texto: "São a Universalidade, Integralidade e Equidade, que defendem o direito de todos à saúde com foco na prevenção e no cuidado completo do cidadão.",
                 afirmacao: ["Você é uma pessoa reflexiva e busca aprendizado e conhecimento sobre os direitos sociais e a estrutura que protege a população.",
                            "Você demonstra um perfil consciente e dedicado a compreender os direitos dos cidadãos e os mecanismos públicos de amparo à sociedade."
+                ]
             },
             {
                 texto:  "São os pilares que garantem atendimento gratuito a quem precisa, prestando atenção especial às desigualdades para proteger os mais vulneráveis.",
-                afirmacao: "Você é uma pessoa observadora e empática, pois se preocupa diretamente com o bem-estar do próximo e com a justiça social na prática.",
+                afirmacao: ["Você é uma pessoa observadora e empática, pois se preocupa diretamente com o bem-estar do próximo e com a justiça social na prática.",
                            "Você revela um perfil sensível e atento às necessidades dos outros, priorizando a igualdade de oportunidades e o acolhimento humano no dia a dia."
+                ]
             }    
            
         ]
@@ -20,14 +22,16 @@ export const perguntas = [
             enunciado: "A saúde pública apoia-se em estratégias coletivas para evitar a proliferação de doenças. As campanhas de vacinação em massa são apontadas historicamente como a forma mais eficaz de erradicar epidemias. Diante disso, qual a sua visão sobre o investimento contínuo em imunização?",
             alternativas: [
                 {
-                    texto: "O investimento em vacinas deve ser máximo e obrigatório para toda a população, pois a proteção coletiva e a erradicação de vírus salvam o sistema de saúde de colapsos.",
+                    texto: ["O investimento em vacinas deve ser máximo e obrigatório para toda a população, pois a proteção coletiva e a erradicação de vírus salvam o sistema de saúde de colapsos.",
                     afirmacao: "Você demonstra um perfil consciente e dedicado a compreender os direitos dos cidadãos e os mecanismos públicos de amparo à sociedade.",
+                    ]
                                "Sua postura é analítica e voltada para o entendimento das garantias sociais e das políticas criadas para a proteção coletiva."
                 },
                 {
-                    texto:  "As campanhas devem focar prioritariamente na conscientização e no diálogo, avaliando os custos de distribuição antes de impor regras rígidas para toda a população.",
+                    texto:  ["As campanhas devem focar prioritariamente na conscientização e no diálogo, avaliando os custos de distribuição antes de impor regras rígidas para toda a população.",
                     afirmacao: "Você revela um perfil sensível e atento às necessidades dos outros, priorizando a igualdade de oportunidades e o acolhimento humano no dia a dia.",
                                "Sua postura é acolhedora e focada em perceber a realidade ao redor, mostrando um forte compromisso com a dignidade humana e o amparo social."
+                    ]
                 }    
                
             ]
@@ -37,14 +41,16 @@ export const perguntas = [
 
             alternativas: [
                 {
-                    texto: "O governo deve universalizar o saneamento básico imediatamente em todas as periferias, entendendo que cada real gasto em água limpa economiza custos em tratamentos médicos futuros.",
+                    texto: ["O governo deve universalizar o saneamento básico imediatamente em todas as periferias, entendendo que cada real gasto em água limpa economiza custos em tratamentos médicos futuros.",
                     afirmacao: "Você possui um perfil questionador e busca se aprofundar no conhecimento sobre os deveres do Estado e o bem-estar da população.",
                                "Você demonstra uma mente focada no futuro, que aposta no planejamento público como a principal chave para o desenvolvimento social."
+                    ]
                 },
                 {
-                    texto:    "A melhoria do saneamento deve ser feita de forma gradual e planejada por bairros, garantindo que o orçamento público atual não fique sobrecarregado no curto prazo."
+                    texto:    ["A melhoria do saneamento deve ser feita de forma gradual e planejada por bairros, garantindo que o orçamento público atual não fique sobrecarregado no curto prazo."
                     afirmacao: "Você demonstra um perfil solidário e perspicaz, que se importa com a proteção dos mais vulneráveis e com a aplicação prática da equidade.",
                                "Sua perspectiva é realista e equilibrada, demonstrando preocupação com a eficiência do projeto e com a viabilidade econômica imediata."
+                    ]
                 }    
                
             ]
